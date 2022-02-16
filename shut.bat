b81122042ed76879ecbf200f3ae57a2a0c34adfa
aissaabk <belmelahmed@gmail.com>
2022-02-16 10:31:32 +0100

    Add files via upload

---
+++ shut.bat
@@ -0,0 +1,15 @@
+@echo off
+echo this script used for shutdown after check if your app is not running or after time xxx min
+echo how you do for shutdown your pc
+echo 1- After a specified time
+echo 2- After completing the tasks of the specified program
+set /p id="Enter choice: "
+echo %id%
+if %id% EQU 1 (set /p time="Enter time in min : "&&set /a "timemin=time*60"&&echo Pc shutdown after %time% min &&timeout 10&& goto shutdown ) else ( set /p app="enter name application complete like Viber.exe : ")
+echo %app%
+:loop
+tasklist /fi "USERNAME ne NT AUTHORITY\SYSTEM" /fi "STATUS eq running" | find  "%app%"
+if "%ERRORLEVEL%"=="0" (cls &&echo %app% is running&&goto loop) else (cls&&echo pc shutdown after 20s &&echo timeout 20 &&shutdown /s /t 10 ) 
+:shutdown 
+shutdown /s /t %timemin%
+
